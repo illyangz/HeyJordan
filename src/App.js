c23c2fed30fa2b8f3ab2384c759b1793d88198e3
Hassan Mango <91436000+illyangz@users.noreply.github.com>
2024-10-28 13:04:00 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import * as THREE from 'three'
+import { useRef, useState } from 'react'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { useIntersect, Image, ScrollControls, Scroll } from '@react-three/drei'
+
+function Item({ url, scale, ...props }) {
+  const visible = useRef(false)
+  const [hovered, hover] = useState(false)
+  const ref = useIntersect((isVisible) => (visible.current = isVisible))
+  const { height } = useThree((state) => state.viewport)
+  useFrame((state, delta) => {
+    ref.current.position.y = THREE.MathUtils.damp(ref.current.position.y, visible.current ? 0 : -height / 2 + 1, 4, delta)
+    ref.current.material.zoom = THREE.MathUtils.damp(ref.current.material.zoom, visible.current ? 1 : 1.5, 4, delta)
+    ref.current.material.grayscale = THREE.MathUtils.damp(ref.current.material.grayscale, hovered ? 1 : 0, 4, delta)
+  })
+  return (
+    <group {...props}>
+      <Image ref={ref} onPointerOver={() => hover(true)} onPointerOut={() => hover(false)} scale={scale} url={url} />
+    </group>
+  )
+}
+
+function Items() {
+  const { width: w, height: h } = useThree((state) => state.viewport)
+  return (
+    <Scroll>
+      <Item url="/1.jpg" scale={[w / 3, w / 3, 1]} position={[-w / 6, 0, 0]} />
+      <Item url="/2.jpg" scale={[2, w / 3, 1]} position={[w / 30, -h, 0]} />
+      <Item url="/3.jpg" scale={[w / 3, w / 5, 1]} position={[-w / 4, -h * 1, 0]} />
+      <Item url="/4.jpg" scale={[w / 5, w / 5, 1]} position={[w / 4, -h * 1.2, 0]} />
+      <Item url="/5.jpg" scale={[w / 5, w / 5, 1]} position={[w / 10, -h * 1.75, 0]} />
+      <Item url="/6.jpg" scale={[w / 3, w / 3, 1]} position={[-w / 4, -h * 2, 0]} />
+      <Item url="/7.jpg" scale={[w / 3, w / 5, 1]} position={[-w / 4, -h * 2.6, 0]} />
+      <Item url="/8.jpg" scale={[w / 2, w / 2, 1]} position={[w / 4, -h * 3.1, 0]} />
+      <Item url="/12.jpg" scale={[w / 2.5, w / 2, 1]} position={[-w / 6, -h * 4.1, 0]} />
+    </Scroll>
+  )
+}
+
+export const App = () => (
+  <Canvas orthographic camera={{ zoom: 80 }} gl={{ alpha: false, antialias: false, stencil: false, depth: false }} dpr={[1, 1.5]}>
+    <color attach="background" args={['#f0f0f0']} />
+    <ScrollControls damping={6} pages={7}>
+      <Items />
+      <Scroll html style={{ width: '100%' }}>
+        <h1 style={{ position: 'absolute', top: `100vh`, right: '20vw', fontSize: '10em', transform: `translate3d(20%,-100%,0)` }}>
+          hey jordan
+        </h1>
+        <h1 style={{ position: 'absolute', top: '180vh', left: '10vw' }}>this is not my get out of probation for free card :)</h1>
+        <h1 style={{ position: 'absolute', top: '260vh', right: '10vw' }}>
+          I feel like I've been numb for a while and I'm sorry for not being more present and disciplined.{' '}
+        </h1>
+        <h1 style={{ position: 'absolute', top: '350vh', left: '10vw' }}>
+          {' '}
+          I made this with some of the things I've been learning and putting to work. And bcs you are on my mind hehe{' '}
+        </h1>
+        <h1 style={{ position: 'absolute', top: '450vh', left: '10vw' }}>
+          I enjoy spending time with you and I want to fortify that.
+          {'<3'}
+        </h1>
+
+        <h1 style={{ position: 'absolute', top: '560vh', right: '10vw' }}>
+          {'<3'}
+          <br />
+          yanga
+        </h1>
+      </Scroll>
+    </ScrollControls>
+  </Canvas>
+)
